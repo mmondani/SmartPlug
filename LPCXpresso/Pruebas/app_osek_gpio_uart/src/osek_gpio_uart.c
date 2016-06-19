e2c59e754adaf10b566112a554c37b166ef0d5a2
mmondani <mmondani89@gmail.com>
2016-06-19 11:07:30 -0300

    Se solucionó el problema de la corrupción de las variables aumentando el stack para la tarea TaskInit

--- LPCXpresso/Pruebas/app_osek_gpio_uart/src/osek_gpio_uart.c
+++ LPCXpresso/Pruebas/app_osek_gpio_uart/src/osek_gpio_uart.c
@@ -3,8 +3,14 @@
  Name        : osek_gpio_uart.c
  Author      : Mariano Mondani
  Version     :
- Copyright   : (c)2014, Mariano Mondani
- Description :
+ Copyright   : (c)2016, Mariano Mondani
+ Description : Existen 3 tareas:
+ 	 - InputManager: se encarga de muestrear 3 switches y generar los eventos
+ 	 	 	 	 	 cuando se los presiona.
+ 	 - Sender1: cuando se presiona el switch 1 o 2 se envía un mensaje por
+ 	 	 	 	la UART informándolo.
+ 	 - Sender2: cuando se presiona el switch 2 o 3 se envía un mensaje por
+ 	 	 	 	la UART informándolo.
 ===============================================================================
 */
 
@@ -52,16 +58,6 @@ int main(void)
 	SystemCoreClockUpdate();
 	initMemHeap();
 
-	uart = cObject_new(ioUART, LPC_UART3, IOUART_BR_9600, IOUART_DATA_8BIT, IOUART_PAR_NONE, IOUART_STOP_1BIT, IOUART_MODE_NON_BLOCKING, 100, 10);
-    ioObject_init(uart);
-    ioComm_intEnable(uart, IOUART_INT_TX);
-    //ioComm_intEnable(uart3, IOUART_INT_RX);
-    ioUART_configFIFO(uart, IOUART_FIFO_LEVEL0);
-
-
-    NVIC_SetPriority(UART3_IRQn, 1);
-    NVIC_EnableIRQ(UART3_IRQn);
-
 
 	// Comienza el RTOS en el modo AppMode1
     StartOS(AppMode1);
@@ -81,11 +77,18 @@ void ErrorHook(void)
 
 TASK(TaskInit)
 {
+	uart = cObject_new(ioUART, LPC_UART3, IOUART_BR_9600, IOUART_DATA_8BIT, IOUART_PAR_NONE, IOUART_STOP_1BIT, IOUART_MODE_NON_BLOCKING, 100, 10);
+    ioObject_init(uart);
+    ioComm_intEnable(uart, IOUART_INT_TX);
+    //ioComm_intEnable(uart3, IOUART_INT_RX);
+    ioUART_configFIFO(uart, IOUART_FIFO_LEVEL0);
+
+    NVIC_SetPriority(UART3_IRQn, 1);
+    NVIC_EnableIRQ(UART3_IRQn);
+
+
+
 	tskInputManager_init();
-
-
-
-
 	tskSender1_init(uart);
 	tskSender2_init(uart);
 
