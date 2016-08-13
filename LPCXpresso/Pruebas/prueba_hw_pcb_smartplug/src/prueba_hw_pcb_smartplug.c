e49031e31aba157b756f5752e0057c5bb75f6417
mmondani <mmondani89@gmail.com>
2016-08-12 23:02:53 -0300

    Se agrega a la prueba de hardware del PCB del Smart Plug el CS5490.
    Inicia el CS5490 y cada 1 segundo imprime por la UART de debug los parámetros leídos del CS5490 y del RTC interno.
    Al presionar el pulsador se muestra solo la hora.
    Los leds titilan cada 1 segundo.

--- LPCXpresso/Pruebas/prueba_hw_pcb_smartplug/src/prueba_hw_pcb_smartplug.c
+++ LPCXpresso/Pruebas/prueba_hw_pcb_smartplug/src/prueba_hw_pcb_smartplug.c
@@ -21,6 +21,8 @@
 #include "ioDebounce.h"
 #include "ioUART.h"
 #include "ioInternalRTC.h"
+#include "ioCS5490.h"
+#include "cTimer.h"
 
 
 void* pinLedVerde;
@@ -29,16 +31,30 @@ void* pinSwitch;
 void* debounceSwitch;
 void* uartDebug;
 void* rtc;
+void* gpioDO;
+void* gpioReset;
+void* uartCS5490;
+void* cs5490;
+void* refreshTimer;
 
 uint32_t tickCount;
+uint32_t pulsos;
+
+
+void term_clear (void* uart);
+void term_home (void* uart);
+
+
 
 
 void SysTick_Handler(void)
 {
+	cTimer_handler();
+
 	ioDebounce_handler(debounceSwitch);
 
 	tickCount ++;
-	if (tickCount >= 10)
+	if (tickCount >= 1000)
 	{
 		// Cada 1 seg
 		tickCount = 0;
@@ -62,9 +78,35 @@ void UART1_IRQHandler(void)
 }
 
 
+void UART3_IRQHandler(void)
+{
+	if (ioComm_getInt(uartCS5490, IOUART_INT_ID_TX))
+	{
+		ioUART_txHandler(uartCS5490);
+	}
+	else if(ioComm_getInt(uartCS5490, IOUART_INT_ID_RX))
+	{
+		ioUART_rxHandler(uartCS5490);
+	}
+}
+
+
+void EINT3_IRQHandler(void)
+{
+	Chip_GPIOINT_ClearIntStatus(LPC_GPIOINT, GPIOINT_PORT0, 1 << 27);
+	pulsos++;
+}
+
+
+
+
 int main(void) {
 	rtc_time_t fullTime;
 	uint8_t buff[50];
+	uint32_t temperatura, muestras, irms, vrms, potencia_activa, potencia_reactiva, potencia_aparente;
+	uint32_t i_offset, v_offset, i_ganancia, v_ganancia, factor_potencia, escala, epsilon, settle;
+	float conversion, irms_linea, vrms_linea, potencia_activa_linea, potencia_reactiva_linea, frecuencia_linea;
+	float potencia_aparente_linea;
 
 
     // Read clock settings and update SystemCoreClock variable
@@ -73,19 +115,26 @@ int main(void) {
 
     initMemHeap();
 
+
+    // ===================== [LEDS] =====================
     pinLedVerde = cObject_new(ioDigital, LPC_GPIO, IOGPIO_OUTPUT, 0, 21);
     ioObject_init(pinLedVerde);
 
     pinLedRojo = cObject_new(ioDigital, LPC_GPIO, IOGPIO_OUTPUT, 2, 13);
     ioObject_init(pinLedRojo);
+    // =====================================================
 
+
+    // ===================== [SWITCH] =====================
     pinSwitch = cObject_new(ioDigital, LPC_GPIO, IOGPIO_INPUT, 0, 3);
     ioObject_init(pinSwitch);
 
-    debounceSwitch = cObject_new(ioDebounce, pinSwitch, IODIGITAL_LEVEL_HIGH, 4);
+    debounceSwitch = cObject_new(ioDebounce, pinSwitch, IODIGITAL_LEVEL_HIGH, 40);
+    // =====================================================
 
 
-    uartDebug = cObject_new(ioUART, LPC_UART1, IOUART_BR_9600, IOUART_DATA_8BIT, IOUART_PAR_NONE, IOUART_STOP_1BIT, IOUART_MODE_NON_BLOCKING, 50, 50);
+    // ===================== [UART DEBUG] =====================
+    uartDebug = cObject_new(ioUART, LPC_UART1, IOUART_BR_115200, IOUART_DATA_8BIT, IOUART_PAR_NONE, IOUART_STOP_1BIT, IOUART_MODE_BLOCKING, 2, 10);
     ioObject_init(uartDebug);
     ioComm_intEnable(uartDebug, IOUART_INT_TX);
     ioComm_intEnable(uartDebug, IOUART_INT_RX);
@@ -93,7 +142,39 @@ int main(void) {
 
     NVIC_SetPriority(UART1_IRQn, 1);
     NVIC_EnableIRQ(UART1_IRQn);
+    // =====================================================
 
+
+    // ===================== [SYS TICK] =====================
+    SysTick_Config(SystemCoreClock / 1000);
+    // =====================================================
+
+
+    // ===================== [CS5490] =====================
+    gpioDO = cObject_new(ioDigital, LPC_GPIO, IOGPIO_INPUT, 0, 27);
+    ioObject_init(gpioDO);
+    Chip_GPIOINT_SetIntFalling(LPC_GPIOINT, GPIOINT_PORT0, 1 << 27);
+    NVIC_ClearPendingIRQ(EINT3_IRQn);
+    NVIC_EnableIRQ(EINT3_IRQn);
+
+    gpioReset = cObject_new(ioDigital, LPC_GPIO, IOGPIO_OUTPUT, 1, 30);
+    ioObject_init(gpioReset);
+
+    uartCS5490 = cObject_new(ioUART, LPC_UART3, IOUART_BR_600, IOUART_DATA_8BIT, IOUART_PAR_NONE, IOUART_STOP_1BIT, IOUART_MODE_BLOCKING, 2, 20);
+	ioObject_init(uartCS5490);
+	ioComm_intEnable(uartCS5490, IOUART_INT_TX);
+	ioComm_intEnable(uartCS5490, IOUART_INT_RX);
+	ioUART_configFIFO(uartCS5490, IOUART_FIFO_LEVEL0);
+
+	NVIC_SetPriority(UART3_IRQn, 1);
+	NVIC_EnableIRQ(UART3_IRQn);
+
+	cs5490 = cObject_new (ioCS5490, uartCS5490, gpioReset, 4000.0, 220.0, 5.0, 1.75, 5000.0, 8.0);
+	ioCS5490_init(cs5490, 0, 0, 0x400000, 0x400000);
+	// =====================================================
+
+
+	// ===================== [RTC] =====================
     rtc = cObject_new(ioInternalRTC, LPC_RTC);
     ioObject_init(rtc);
 
@@ -107,14 +188,16 @@ int main(void) {
     fullTime.year = 2016;
 
     ioRTC_setFullTime(rtc, &fullTime);
-
     ioObject_enable(rtc);
+    // =================================================
 
 
-    SysTick_Config(SystemCoreClock / 100);
+    refreshTimer = cObject_new(cTimer);
+    cTimer_start(refreshTimer, 1000);
 
     ioObject_write(pinLedVerde, 1);
     ioObject_write(pinLedRojo, 0);
+
 
     while(1)
     {
@@ -122,13 +205,125 @@ int main(void) {
     	{
     		ioRTC_getFullTime(rtc, &fullTime);
 
+			term_clear(uartDebug);
+			term_home(uartDebug);
+
     		sprintf(buff, "%.2d:%.2d:%.2d  -  %.2d/%.2d/%.4d  - %.1d\n\r", fullTime.hour, fullTime.minute, fullTime.second,
     																		fullTime.dayOfMonth, fullTime.month, fullTime.year,
 																			fullTime.dayOfWeek);
     		ioUART_writeString(uartDebug, buff);
     	}
+
+
+    	if (cTimer_hasExpired(refreshTimer))
+		{
+			cTimer_start(refreshTimer, 2000);
+
+			ioCS5490_pageSelect(cs5490, IOCS5490_PAGE_16);
+			temperatura = ioCS5490_registerRead(cs5490, IOCS5490_REG_T_REG);
+			muestras = ioCS5490_registerRead(cs5490, IOCS5490_REG_TIME);
+			irms = ioCS5490_registerRead(cs5490, IOCS5490_REG_I_RMS);
+			vrms = ioCS5490_registerRead(cs5490, IOCS5490_REG_V_RMS);
+			potencia_activa = ioCS5490_registerRead(cs5490, IOCS5490_REG_P_AVG);
+			potencia_reactiva = ioCS5490_registerRead(cs5490, IOCS5490_REG_Q_AVG);
+			potencia_aparente = ioCS5490_registerRead(cs5490, IOCS5490_REG_S_REG);
+			factor_potencia = ioCS5490_registerRead(cs5490, IOCS5490_REG_PF);
+			epsilon = ioCS5490_registerRead(cs5490, IOCS5490_REG_EPSILON);
+			i_offset = ioCS5490_registerRead(cs5490, IOCS5490_REG_I_DCOFF);
+			v_offset = ioCS5490_registerRead(cs5490, IOCS5490_REG_V_DCOFF);
+			i_ganancia = ioCS5490_registerRead(cs5490, IOCS5490_REG_I_GAIN);
+			v_ganancia = ioCS5490_registerRead(cs5490, IOCS5490_REG_V_GAIN);
+			settle = ioCS5490_registerRead(cs5490, IOCS5490_REG_T_SETTLE);
+
+			ioCS5490_pageSelect(cs5490, IOCS5490_PAGE_18);
+			escala = ioCS5490_registerRead(cs5490, IOCS5490_REG_SCALE);
+
+
+			term_clear(uartDebug);
+			term_home(uartDebug);
+
+			conversion = ioCS5490_signedFract2Float(temperatura, 7, 16);
+			sprintf(buff, "Temperatura: %f\n\r", conversion);
+			ioUART_writeString(uartDebug, buff);
+			sprintf(buff, "Muestras: %lX\n\r", muestras);
+			ioUART_writeString(uartDebug, buff);
+
+			conversion = ioCS5490_unsignedFract2Float(irms, 0, 24);
+			irms_linea = conversion * ioCS5490_getIcalibration(cs5490) / ioCS5490_getMeterScale(cs5490);
+			sprintf(buff, "IRMS: %f\t\t%f\n\r", conversion, irms_linea);
+			ioUART_writeString(uartDebug, buff);
+
+			conversion = ioCS5490_unsignedFract2Float(vrms, 0, 24);
+			vrms_linea = conversion * ioCS5490_getVmax(cs5490) / 0.6;
+			sprintf(buff, "VRMS: %f\t\t%f\n\r", conversion, vrms_linea);
+			ioUART_writeString(uartDebug, buff);
+
+			conversion = ioCS5490_signedFract2Float(potencia_activa, 0, 23);
+			potencia_activa_linea = conversion * ioCS5490_getMaxPower(cs5490) / ioCS5490_getPowerScale(cs5490);
+			sprintf(buff, "Potencia activa: %f\t\t%f\n\r", conversion, potencia_activa_linea);
+			ioUART_writeString(uartDebug, buff);
+
+			conversion = ioCS5490_signedFract2Float(potencia_reactiva, 0, 23);
+			potencia_reactiva_linea = conversion * ioCS5490_getMaxPower(cs5490) / ioCS5490_getPowerScale(cs5490);
+			sprintf(buff, "Potencia reactiva: %f\t\t%f\n\r", conversion, potencia_reactiva_linea);
+			ioUART_writeString(uartDebug, buff);
+
+			conversion = ioCS5490_signedFract2Float(potencia_aparente, 0, 23);
+			potencia_aparente_linea = conversion * ioCS5490_getMaxPower(cs5490) / ioCS5490_getPowerScale(cs5490);
+			sprintf(buff, "Potencia aparente: %f\t\t%f\n\r", conversion, potencia_aparente_linea);
+			ioUART_writeString(uartDebug, buff);
+
+			conversion = ioCS5490_signedFract2Float(factor_potencia, 0, 23);
+			sprintf(buff, "Factor de potencia: %f\n\r", conversion);
+			ioUART_writeString(uartDebug, buff);
+
+			conversion = ioCS5490_signedFract2Float(epsilon, 0, 23);
+			frecuencia_linea = conversion * ioCS5490_getWordRate(cs5490);
+			sprintf(buff, "Frecuencia linea: %f\t\t%f\n\r", conversion, frecuencia_linea);
+			ioUART_writeString(uartDebug, buff);
+
+			conversion = ioCS5490_signedFract2Float(i_offset, 0, 23);
+			sprintf(buff, "I DC Offset: %f\n\r", conversion);
+			ioUART_writeString(uartDebug, buff);
+
+			conversion = ioCS5490_signedFract2Float(v_offset, 0, 23);
+			sprintf(buff, "V DC Offset: %f\n\r", conversion);
+			ioUART_writeString(uartDebug, buff);
+
+			conversion = ioCS5490_unsignedFract2Float(i_ganancia, 2, 22);
+			sprintf(buff, "I Ganancia: %f\n\r", conversion);
+			ioUART_writeString(uartDebug, buff);
+
+			conversion = ioCS5490_unsignedFract2Float(v_ganancia, 2, 22);
+			sprintf(buff, "V Ganancia: %f\n\r", conversion);
+			ioUART_writeString(uartDebug, buff);
+
+			sprintf(buff, "Pulsos: %d\n\r", pulsos);
+			ioUART_writeString(uartDebug, buff);
+
+
+			ioRTC_getFullTime(rtc, &fullTime);
+			sprintf(buff, "%.2d:%.2d:%.2d  -  %.2d/%.2d/%.4d  - %.1d\n\r", fullTime.hour, fullTime.minute, fullTime.second,
+																			fullTime.dayOfMonth, fullTime.month, fullTime.year,
+																			fullTime.dayOfWeek);
+			ioUART_writeString(uartDebug, buff);
+		}
     }
 
 
     return 0 ;
+}
+
+
+void term_clear (void* uart)
+{
+	ioObject_write(uart, 0x1B);
+	ioComm_writeBytes(uart, 3, "[2J");
+}
+
+
+void term_home (void* uart)
+{
+	ioObject_write(uart, 0x1B);
+	ioComm_writeBytes(uart, 2, "[H");
 }
