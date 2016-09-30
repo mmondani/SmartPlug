0766ff337fda8ac0092f0215d5f19e0ef1858b26
mmondani <mmondani89@gmail.com>
2016-09-30 17:57:31 -0300

    En SmartPlugApp se termina la lógica de HistorySelectionFragment. Cuando se elije la fecha de uno de los spinners ahora se muestra un Toast. Finalmente va a llamar al Fragment con el plot.

--- Android_App/SmartPlugApp/app/src/main/java/com/company/smartplugapp/HistorySelectionFragment.java
+++ Android_App/SmartPlugApp/app/src/main/java/com/company/smartplugapp/HistorySelectionFragment.java
@@ -7,8 +7,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,6 +25,8 @@ public class HistorySelectionFragment extends Fragment {
 
     private Spinner mPowerDateSpinner;
     private Spinner mEnergyDateSpinner;
+    List<String> mPowerSpinnerElements;
+    List<String> mEnergySpinnerElements;
 
     private String mId;
 
@@ -73,21 +77,23 @@ public class HistorySelectionFragment extends Fragment {
         List<MeasurementsEntry> energyMeasurementsEntries = SmartPlugProvider.getInstance(getActivity()).
                 getMeasurementsEntries(mId, MeasurementsEntry.MeasurementType.ENERGY);
 
-        List<String> powerSpinnerElements = new ArrayList<>();
-        List<String> energySpinnerElements = new ArrayList<>();
+        mPowerSpinnerElements = new ArrayList<>();
+        mEnergySpinnerElements = new ArrayList<>();
 
 
         if (powerMeasurementsEntries != null && energyMeasurementsEntries != null) {
+            mPowerSpinnerElements.add("Elija una fecha");
             for (MeasurementsEntry entry : powerMeasurementsEntries)
-                powerSpinnerElements.add(entry.getDate());
+                mPowerSpinnerElements.add(entry.getDate());
 
+            mEnergySpinnerElements.add("Elija una fecha");
             for (MeasurementsEntry entry : energyMeasurementsEntries)
-                energySpinnerElements.add(entry.getDate());
+                mEnergySpinnerElements.add(entry.getDate());
         }
 
 
-        ArrayAdapter<String> powerSpinnerAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, powerSpinnerElements);
-        ArrayAdapter<String> energySpinnerAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, energySpinnerElements);
+        ArrayAdapter<String> powerSpinnerAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, mPowerSpinnerElements);
+        ArrayAdapter<String> energySpinnerAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, mEnergySpinnerElements);
 
         powerSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         energySpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -95,6 +101,44 @@ public class HistorySelectionFragment extends Fragment {
 
         mPowerDateSpinner.setAdapter(powerSpinnerAdapter);
         mEnergyDateSpinner.setAdapter(energySpinnerAdapter);
+
+        mPowerDateSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                /**
+                 * Este método también se llama cuando el Spinner es creado (position = 0). De esta forma
+                 * se evita llamar al Fragment del plot cuando se crea el Spinner.
+                 */
+                if (position != 0) {
+                    Toast.makeText(getActivity(), mPowerSpinnerElements.get(position), Toast.LENGTH_SHORT).show();
+                    /** TODO Remover el Toast y llamar al Fragment que muestre el plot */
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        mEnergyDateSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                /**
+                 * Este método también se llama cuando el Spinner es creado (position = 0). De esta forma
+                 * se evita llamar al Fragment del plot cuando se crea el Spinner.
+                 */
+                if (position != 0) {
+                    Toast.makeText(getActivity(), mEnergySpinnerElements.get(position), Toast.LENGTH_SHORT).show();
+                    /** TODO Remover el Toast y llamar al Fragment que muestre el plot */
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         //getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
